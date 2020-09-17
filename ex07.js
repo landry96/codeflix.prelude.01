cbf8d409374039dc4c6ab8b5d6b35ae55718298a
landry96 <55443238+landry96@users.noreply.github.com>
2020-09-17 14:51:34 +0200

    Converts the subject to kebab case, also called spinal case or lisp case.

---
+++ ex07.js
@@ -0,0 +1,15 @@
+function snakeCase(str =''){
+    let sentence = '' ;
+    for (i=0 , i< str.length; i++ ){
+        const code = str.charCodeAt(i);
+        //console.log(code);
+        if (code )
+
+    }
+
+}
+
+
+console.log(snakeCase('gold d roger'));
+console.log(snakeCase('GoldDoger'));
+console.log(snakeCase('-Golder-D-Roger-'));
