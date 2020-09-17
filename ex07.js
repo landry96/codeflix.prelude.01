cc977bd8333da349ca6208e78d7c217ba42d59dc
landry96 <55443238+landry96@users.noreply.github.com>
2020-09-17 15:57:57 +0200

    Converts the subject to snake case

--- ex07.js
+++ ex07.js
@@ -1,15 +1,36 @@
-function snakeCase(str =''){
-    let sentence = '' ;
-    for (i=0 , i< str.length; i++ ){
-        const code = str.charCodeAt(i);
-        //console.log(code);
-        if (code )
-
+function isUpperCase(code) {
+    return (code >= 65 && code <= 90)
+  }
+  
+  function snakeCase(str= ''){
+    let sentence = ''
+  
+    for (let i = 0; i < str.length; i++) {
+      const code = str.charCodeAt(i)
+  
+      if (code === 45) {
+        // Get rid of - from the beginning and ending of a string
+        continue
+      }
+  
+      if (code === 32) {
+        // Replace spaces with -
+        sentence += "_"
+      } else if (isUpperCase(code)) {
+        if (i > 1) {
+          // Add - before uppercase
+          sentence += "_"
+        }
+  
+        sentence += String.fromCharCode(code + 32)
+      } else {
+        sentence += str[i]
+      }
     }
-
-}
-
-
-console.log(snakeCase('gold d roger'));
-console.log(snakeCase('GoldDoger'));
-console.log(snakeCase('-Golder-D-Roger-'));
+  
+    return sentence
+  }
+  
+  console.log(snakeCase('gold d roger'))
+  console.log(snakeCase('GoldDRoger'))
+  console.log(snakeCase('-Gold-D-Roger-'))
